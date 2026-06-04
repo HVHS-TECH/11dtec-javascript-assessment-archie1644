da6a654e389ea719611d24de31c9b8e139a754cd
archie1644 <24220ac@hvhs.school.nz>
2026-06-04 02:56:53 +0000

    Jun 4, 2026, 2:56 AM

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ javascript
 
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML += "<h2>added by javascrript</h2>";
+
+//variables
+
+
+
+
+
+
+
+
+
+/****************************
+main code
+****************************/
