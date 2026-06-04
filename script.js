e4f5806dcee3af703db8171d487e46c47bef2afd
archie1644 <24220ac@hvhs.school.nz>
2026-06-04 03:03:14 +0000

    Jun 4, 2026, 3:03 AM

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ javascript
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 
 //variables
+function getFormInput(){
+    const NAME_FIELD = document.getElementById("nameField");
+    let userName = NAME_FIELD.value;
+    OUTPUT.innerHTML += "test";
+}
 
-OUTPUT.innerHTML += "test";
 
 
 
