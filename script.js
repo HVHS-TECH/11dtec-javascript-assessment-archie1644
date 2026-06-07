57d92e99b9ce53cd4ffa246fc73571fe81067d9b
archie1644 <24220ac@hvhs.school.nz>
2026-06-07 21:45:05 +0000

    Jun 7, 2026, 9:45 PM

--- script.js
+++ script.js
@@ -6,13 +6,21 @@ const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 
 //variables
 
-
-
 /****************************
 main code
 ****************************/
 OUTPUT.innerHTML +=  "<h1>pizza place</h1>"
 
 function start(){
-    OUTPUT.innerHTML
+    const NAME_FIELD = document.getElementById("nameField")
+    let name = NAME_FIELD.value;
+    const ITEM_FIELD = document.getElementById("itemField")
+    let item = ITEM_FIELD.value;
+    const NAME_FIELD = document.getElementById("moneyField")
+    let money = MONEY_FIELD.value;
+
+
+    OUTPUT.innerHTML += "<p>name: ">name<"";
+    OUTPUT.innerHTML += "<p>name: ">item<"";
+    OUTPUT.innerHTML += "<p>name: ">money<"";
 }
