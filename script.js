41223ea07636825c292dfe9c4e19cd2ad4ca4c0a
archie1644 <24220ac@hvhs.school.nz>
2026-06-07 21:33:17 +0000

    Jun 7, 2026, 9:33 PM

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ main code
 OUTPUT.innerHTML +=  "<h1>pizza place</h1>"
 
 function getFormInput(){
-    const OUTPUT = document.getElementById("JavaScriptOutput")
+    const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
     let name = NAME_FIELD.value;
     OUTPUT.innerHTML += name;
 }
