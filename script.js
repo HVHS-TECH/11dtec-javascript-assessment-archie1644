fcf931d2b091a707aa4c703c74833c7e89b57bc8
archie1644 <24220ac@hvhs.school.nz>
2026-06-08 22:13:03 +0000

    Jun 8, 2026, 10:13 PM

--- script.js
+++ script.js
@@ -12,17 +12,16 @@ main code
 OUTPUT.innerHTML +=  "<h1>pizza place</h1>"
 
 function start(){
-
     const NAME_FIELD = document.getElementById("nameField")
     let name = NAME_FIELD.value;
-    OUTPUT.innerHTML += "<p>name: ">name<"";
-
     const ITEM_FIELD = document.getElementById("itemField")
     let item = ITEM_FIELD.value;
-        OUTPUT.innerHTML += "<p>item: ">item<"";
-
     const MONEY_FIELD = document.getElementById("moneyField")
-    let money = MONEY_FIELD.value;
+    let money = Number(MONEY_FIELD.value);
+
+
+    OUTPUT.innerHTML += "<p>name: ">name<"";
+    OUTPUT.innerHTML += "<p>item: ">item<"";
     OUTPUT.innerHTML += "<p>money: ">money<"";
 
 
