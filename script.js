9c6746e008f955169682979b2a150de360d5358e
archie1644 <24220ac@hvhs.school.nz>
2026-06-08 22:11:14 +0000

    Jun 8, 2026, 10:11 PM

--- script.js
+++ script.js
@@ -13,25 +13,18 @@ OUTPUT.innerHTML +=  "<h1>pizza place</h1>"
 
 function start(){
 
-
-
-function getFormInput(){
     const NAME_FIELD = document.getElementById("nameField")
     let name = NAME_FIELD.value;
     OUTPUT.innerHTML += "<p>name: ">name<"";
-}
 
-function getFormInput(){
     const ITEM_FIELD = document.getElementById("itemField")
     let item = ITEM_FIELD.value;
         OUTPUT.innerHTML += "<p>item: ">item<"";
-}
 
-function getFormInput(){
     const MONEY_FIELD = document.getElementById("moneyField")
     let money = MONEY_FIELD.value;
     OUTPUT.innerHTML += "<p>money: ">money<"";
-}
+
 
 
 }
