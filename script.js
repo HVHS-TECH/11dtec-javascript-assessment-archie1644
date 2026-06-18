f33a651871de639ebc880f47948e6982b084a6f9
archie1644 <24220ac@hvhs.school.nz>
2026-06-18 02:24:14 +0000

    Jun 18, 2026, 2:24 AM

--- script.js
+++ script.js
@@ -9,18 +9,12 @@ const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 /****************************
 main code
 ****************************/
+
+
 displayProduct("<p>pizza1: ", 0)
 displayProduct("<p>pizza2: ", 1)
 displayProduct("<p>pizza3: ", 2)
 displayProduct("<p>pizza4: ", 3)
-
-function start(){
-
-
-
-
-
-
 
 
 
@@ -29,36 +23,52 @@ function start(){
 
 
 //reciept -----------------------------------------------------------------------------------
-function calculateChange(_money, _price){
-let change = _money - _price
-return change;
-}
-if (money < item){
-        OUTPUT.innerHTML += "<p>you do not have enough to buy">item<"."
-    }
-function getFormInput(){
+
+    function getFormInput(){
     const NAME_FIELD = document.getElementById("nameField");
     let name = NAME_FIELD.value;
-    const ITEM_FIELD = document.getElementById("itemField")
+    const ITEM_FIELD = document.getElementById("itemField");
     let item = ITEM_FIELD.value;
-    const MONEY_FIELD = document.getElementById("moneyField")
-    let money = Number(MONEY_FIELD.value);
+    const MONEY_FIELD = document.getElementById("moneyField");
+     let money = Number(MONEY_FIELD.value);
 
-    let itemArray = ["pizza1", "pizza2", "pizza3", "pizza4"];
+     
+
+function start(){
+    if (money < item){
+        OUTPUT.innerHTML += "<p>you do not have enough to buy">item<"."
+    }
+
+    let cartArray = [];
+    cartArray.push(item)
+    OUTPUT.innerHTML += cartArray;
 
 OUTPUT.innerHTML += "<p>Your Receipt: </p>";
 OUTPUT.innerHTML += "<p>Name: " + name + "</p>";
-OUTPUT.innerHTML += "food: "+itemArray[item]+"";
+OUTPUT.innerHTML += "food: "+cartArray[item]+"";
 OUTPUT.innerHTML += "<p>Your remaining balance: " + calculateChange(money, item)  + "</p>";
 }
+}
+
+
+
+
 getFormInput()
 
 
 
-}
+
+
+
+
 
 //functions
 
 function displayProduct(_name, _price){
     OUTPUT.innerHTML += _name + _price
+}
+
+function calculateChange(_money, _price){
+let change = _money - _price
+return change;
 }
