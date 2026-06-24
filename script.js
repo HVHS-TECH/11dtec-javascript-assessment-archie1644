6d160065b9d82afa2a9d00308d527cbdd82ff2f4
archie1644 <24220ac@hvhs.school.nz>
2026-06-24 01:24:01 +0000

    Jun 24, 2026, 1:24 AM

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 main code
 ****************************/
 
-displayProduct("<p>pizza: $", 10);
-displayProduct("<p>pizza2: $", 20);
+displayProduct("<p>pizza: $", 30);
+displayProduct("<p>pizza2: $", 30);
 displayProduct("<p>pizza3: $", 30);
 
 //reciept -----------------------------------------------------------------------------------
@@ -19,11 +19,7 @@ displayProduct("<p>pizza3: $", 30);
 function getFormInput(){
     const NAME_FIELD = document.getElementById("nameField");
     let name = NAME_FIELD.value;
-    const MONEY_FIELD = document.getElementById("moneyField");
-    let money = MONEY_FIELD.value;
     OUTPUT.innerHTML += "<p>your name: "+name+"";
-    OUTPUT.innerHTML += "<p>your money: $"+money+"";
-
 }
 
 
@@ -31,42 +27,37 @@ function getFormInput(){
 function pizza(){
     OUTPUT.innerHTML += "<p>pizza has been added to cart"
     cartArray.push("pizza");
-    money-10;
 }
 
 function pizza2(){
     OUTPUT.innerHTML += "<p>pizza2 has been added to cart"
     cartArray.push("pizza2");
-    money-20;
 }
 
 function pizza3(){
     OUTPUT.innerHTML += "<p>pizza3 has been added to cart"
     cartArray.push("pizza3");
-    money-30;
 
 }
 
 function start(){
     const NAME_FIELD = document.getElementById("nameField");
     let name = NAME_FIELD.value;
-
+    const MONEY_FIELD = document.getElementById("moneyField");
+    let money = Number(MONEY_FIELD.value);
 
 OUTPUT.innerHTML += "<h1>Your reciept:";
 OUTPUT.innerHTML += "<p>Name: "+name+"";
-OUTPUT.innerHTML += "<p>money: "+money+"";
 
 
 for(let i = 0; i < cartArray.length; i++){
     OUTPUT.innerHTML += "item " + (i + 1) + ": " + cartArray[i] + "<br>";
 }
     console.log(cartArray);
-}
 
+OUTPUT.innerHTML += "<p>price: $"+cartArray.length*30+"";
+OUTPUT.innerHTML += "<p>your change: $"+money-cartArray.length*30+"";
 
-function calculateChange(_money, _price){
-let change = _money-_price
-return change;
 }
 
 function displayProduct(_item, _price){
